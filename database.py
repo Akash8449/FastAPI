0549e3f3751d325f5646e776d041b816b904c661
akash-untangl <akash@untangl.net>
2023-12-24 19:36:56 +0530

    first commit

---
+++ database.py
@@ -0,0 +1,21 @@
+from sqlalchemy  import  create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import  declarative_base
+
+# sqlite
+SQLALCHEMY_DATABASE_URL="sqlite:///./todosapp.db"
+
+# postgres
+# SQLALCHEMY_DATABASE_URL= 'postgresql://postgres:test1234!@localhost/TodoApplicationDatabase'
+
+# mysql
+# SQLALCHEMY_DATABASE_URL="mysql+pymysql://root:test1234!@127.0.0.1:3306/TodoApplicationDatabase"
+
+# sqlite
+engine= create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
+
+# mysql and postgres
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base= declarative_base()
